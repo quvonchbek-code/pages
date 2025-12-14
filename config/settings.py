173a1790616ef7a34490c2de52833a2242e4847c
Quvonchbek <boboxonovmirkomil900@gmail.com>
2025-12-14 17:19:29 +0500

    epladik

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s-uq$c*uj)4o=w*yrq8@#b4#xgvev5p_qb+aez+3p7zbwv0g*&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
